fc809203e70bf940c33da88aadbff13e4538ef65
Rajayogan <rajayogan27@gmail.com>
2025-04-22 22:00:43 +0530

    Initial Commit

---
+++ coffeeapp/coffeeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeappApp.swift
+//  coffeeapp
+//
+//  Created by Rajayogan on 22/04/25.
+//
+
+import SwiftUI
+
+@main
+struct coffeeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ coffeeappTests/coffeeappTests.swift
@@ -0,0 +1,17 @@
+//
+//  coffeeappTests.swift
+//  coffeeappTests
+//
+//  Created by Rajayogan on 22/04/25.
+//
+
+import Testing
+@testable import coffeeapp
+
+struct coffeeappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
